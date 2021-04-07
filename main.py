a7028055c131e5fce7eca1761567f67c05fce8b9
vibi-230 <vibin@turftown.in>
2021-04-07 14:28:12 +0530

    working

--- main.py
+++ main.py
@@ -2,13 +2,16 @@ import pandas
 import tkinter as tk
 from tkinter import filedialog
 
-def UploadAction(event=None):
+
+def UploadAction():
     filename = filedialog.askopenfile()
+    data = pandas.read_csv("{}".format(filename.name),skiprows=1)
 
-    data = pandas.read_csv("{}".format(filename.name))
-    sorted_data = data.sort_values(["Venue Name", "Venue Area", "Cost"])
+    sorted_data = pandas.DataFrame(data).sort_values(["Venue Name", "Venue Area", "Cost"])
+    cancelled_refunded_data = sorted_data[(sorted_data["Booking status"] == "cancelled")]
 
-    while len(sorted_data.index) > 0:
+
+    while len(cancelled_refunded_data.index) > 0:
         first_data = sorted_data.head(1)
 
         first_data_venue_name = first_data.iloc[0]["Venue Name"]
@@ -18,10 +21,9 @@ def UploadAction(event=None):
         to_delete_venue_area = to_delete_venue_name[to_delete_venue_name["Venue Area"] == first_data_venue_area]
 
         data_dict = pandas.DataFrame(to_delete_venue_area)
-        data_dict.to_csv("{},{}.csv".format(first_data_venue_name,first_data_venue_area))
+        data_dict.to_csv("{},{}.csv".format(first_data_venue_name, first_data_venue_area))
 
         sorted_data = sorted_data[(sorted_data["Venue Name"] != first_data_venue_name) | (sorted_data["Venue Area"] != first_data_venue_area)]
-        print(to_delete_venue_area)
 
 
 root = tk.Tk()
